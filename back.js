877e28990c49a6d93ea14195c20224c8a39774ef
Sandro Ferroni <sandro.ferroni@epita.fr>
2023-07-06 15:20:17 +0200

    commenter

--- back.js
+++ back.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-app.use(express.json());
-app.use(cors());
+const express = require('express'); // framework création de serveurs
+const cors = require('cors'); // middleware Express : Cross-Origin Resource Sharing (CORS)
+const app = express(); // créer application Express
+app.use(express.json()); // parse format JSON
+app.use(cors()); 
 
-const fs = require("fs");
+const fs = require("fs"); // importe module FileSystem
 
-function readUsersFromJSONFile(JSON_filename) { // Liste des recettes dans le fichier JSON
+// Liste des recettes dans le fichier JSON
+function readUsersFromJSONFile(JSON_filename) {
     const content = fs.readFileSync(JSON_filename, (err) => {
         if (err) {
         console.error(err);
@@ -16,9 +17,10 @@ function readUsersFromJSONFile(JSON_filename) { // Liste des recettes dans le fi
     return JSON.parse(content).recipes;
 }
 
-function writeUsersToJSONFile(JSON_filename, recipes) { // Met a jour la liste des recette dans le JSON
-    const usersJSON = JSON.stringify({ recipes: recipes });
-    fs.writeFileSync(JSON_filename, usersJSON, (err) => {
+// Met a jour la liste des recettes dans le JSON
+function writeUsersToJSONFile(JSON_filename, recipes) {
+    const recipesJSON = JSON.stringify({ recipes: recipes });
+    fs.writeFileSync(JSON_filename, recipesJSON, (err) => {
         if (err) {
         console.error(err);
         return;
@@ -26,6 +28,7 @@ function writeUsersToJSONFile(JSON_filename, recipes) { // Met a jour la liste d
     });
 }
 
+// creation du server
 const server = app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
@@ -39,7 +42,7 @@ app.get('/api/recipes', (req, res) => {
 
 // Post ajouter nouvelle recette
 app.post('/api/recipes', (req, res) => {
-    const { name, description, ingredients, instructions } = req.body;
+    const { name, description, ingredients, instructions } = req.body; // recupere la recette à ajouter
     const createdAt = Date.now();
     const updatedAt = createdAt;
     const new_recipe = {
@@ -58,7 +61,7 @@ app.post('/api/recipes', (req, res) => {
 });
 
 // Get info sur recette id
-app.get('/api/recipes/:id', (req, res) => { //id = name ici
+app.get('/api/recipes/:id', (req, res) => { //id = name recette
     const id = req.params.id;
     let recipes = readUsersFromJSONFile('recipes.json');
     let i = 0;
@@ -75,7 +78,7 @@ app.get('/api/recipes/:id', (req, res) => { //id = name ici
     }
 });
 
-// Put met a jour recette id
+// Put met à jour recette id
 app.put('/api/recipes/:id', (req, res) => {
     const id = req.params.id;
     let recipes = readUsersFromJSONFile('recipes.json');
@@ -95,7 +98,7 @@ app.put('/api/recipes/:id', (req, res) => {
         recipes[i].instructions = instructions;
         recipes[i].updatedAt = Date.now();
         writeUsersToJSONFile('recipes.json',recipes);
-        res.status(200);
+        res.status(201);
         res.send(recipes[i]);
     }
 });
--- front.js
+++ front.js
@@ -1,4 +1,4 @@
-
+// ecouteur d'evenement de l'HTML
 document.addEventListener('DOMContentLoaded', () => {
     const list_id = document.getElementById('list-id');
     const ajout_recette = document.getElementById('nouv-recette');
@@ -7,32 +7,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const btn_maj = document.getElementById('maj-button');
     const btn_annule = document.getElementById('annule-button');
 
-    let recipes = [];
-    let id_recipe = "";
-    let creation = 0;
+    let recipes = []; //liste recettes
+    let id_recipe = ""; // nom de la recette que l'on regarde en detail
+    let creation = 0; // date creation de la recette que l'on regarde en detail
 
+    // affichage de la liste des recettes
     function affichage() {
-        axios.get('http://localhost:3000/api/recipes').then(response => {
+        axios.get('http://localhost:3000/api/recipes').then(response => { // requete recupere liste recettes
             recipes = response.data;
             list_id.innerHTML = '';
             recipes.forEach((recipe, index) => {
                 const list = document.createElement('li');
                 list.classList.add('recipe-item');
+                //affichage des recettes + bouton Voir details et Supprimer la recette
                 list.innerHTML = `
                     <h3>${recipe.name}</h3>
                     <p>${recipe.description}</p>
                     <button class="details-button" data-index="${index}" style="width: 150px; height: 30px;">Voir les détails</button>
                     <button class="delete-button" data-index="${index}" style="width: 150px; height: 30px;">Supprimer la recette</button>`;
-                list_id.appendChild(list);
+                list_id.appendChild(list); //ajout a HTML
             });
-            const btn_detail = document.querySelectorAll('.details-button');
+
+            const btn_detail = document.querySelectorAll('.details-button'); //bouton Voir Details
             btn_detail.forEach(button => {
                 button.addEventListener('click', (event) => {
                     const index = event.target.dataset.index;
                     affich_details(recipes[index].name);
                 });
             });
-            const btn_delete = document.querySelectorAll('.delete-button');
+
+            const btn_delete = document.querySelectorAll('.delete-button'); // bouton Supprimer recette
             btn_delete.forEach((button) => {
                 button.addEventListener('click', (event) => {
                     const index = event.target.dataset.index;
@@ -45,31 +49,36 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // Supprimer une recette
     function supprimerRecette(nom,index) {
-        axios.delete('http://localhost:3000/api/recipes/'+nom)
+        axios.delete('http://localhost:3000/api/recipes/'+nom) //requete
             .then(response => {
                 recipes.splice(index, 1);
             })
             .catch(error => {
                 console.error('Error delete '+nom+' :', error);
             });
-        affichage();
+        affichage(); //met a jour l'affichage des recettes
     }
 
+    // affichage des details
     function affich_details(nom) {
-        id_recipe = nom;
-        axios.get('http://localhost:3000/api/recipes/'+nom)
+        id_recipe = nom; // stock nom de la recette que l'on regarde
+        axios.get('http://localhost:3000/api/recipes/'+nom) //requete
             .then(response => {
-                const recipe = response.data;
-                creation = recipe.createdAt;
+                const recipe = response.data; // recupere donnee de la recette
+                creation = recipe.createdAt; // stock date de creation (sert pour mise a jour)
+                // affiche details
                 document.getElementById('name-details').value = recipe.name;
                 document.getElementById('description-details').value = recipe.description;
                 document.getElementById('ingredients-details').value = recipe.ingredients.join('\n');
                 document.getElementById('instructions-details').value = recipe.instructions;
                 details.style.display = 'block';
+                // affiche ou non les boutons
                 btn_modif.style.display = 'block';
                 btn_maj.style.display = 'none';
                 btn_annule.style.display = 'none';
+                // Ne pas modifier = lecture seulement
                 document.getElementById('name-details').readOnly = true;
                 document.getElementById('description-details').readOnly = true;
                 document.getElementById('ingredients-details').readOnly = true;
@@ -80,26 +89,33 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     }
 
+    // Bouton Modifier
     function modif_details() {
+        // enelever la lecture seulement
         document.getElementById('name-details').readOnly = false;
         document.getElementById('description-details').readOnly = false;
         document.getElementById('ingredients-details').readOnly = false;
         document.getElementById('instructions-details').readOnly = false;
+        // change l'affichage des boutons
         btn_modif.style.display = 'none';
         btn_maj.style.display = 'block';
         btn_annule.style.display = 'block';
     }
 
+    // Bouton annuler = reviens comme avant
     function annule_modif() {
         affich_details(id_recipe);
     }
 
+    // Mise a jour des donnees de la recette
     function maj_modif() {
+        // recupere nouvelles valeurs
         const name = document.getElementById('name-details').value;
         const description = document.getElementById('description-details').value;
         const ingredients = document.getElementById('ingredients-details').value.split('\n');
         const instructions = document.getElementById('instructions-details').value;
-        annule_modif();
+        annule_modif(); // remet l'affichage standart
+        // nouvelle recette mise a jour
         const maj_recette = {
             name,
             description,
@@ -109,7 +125,7 @@ document.addEventListener('DOMContentLoaded', () => {
             updatedAt: Date.now()
         };
 
-        axios.put('http://localhost:3000/api/recipes/'+id_recipe, maj_recette)
+        axios.put('http://localhost:3000/api/recipes/'+id_recipe, maj_recette) //requete pour mettre a jour le json
             .then(response => {
                 affich_details(response.data.name);
                 affichage();
@@ -120,8 +136,10 @@ document.addEventListener('DOMContentLoaded', () => {
         
     }
 
-    ajout_recette.addEventListener('submit', (event) => {
+    // Ajout d'une recette
+    ajout_recette.addEventListener('submit', (event) => { // Probleme si meme name que deja existant = A TRAITER
         event.preventDefault();
+        // recupere les valeurs
         const name = document.getElementById('name').value;
         const description = document.getElementById('description').value;
         const ingredients = document.getElementById('ingredients').value.split(',');
@@ -135,7 +153,7 @@ document.addEventListener('DOMContentLoaded', () => {
             updatedAt: Date.now()
         };
 
-        axios.post('http://localhost:3000/api/recipes', new_recette)
+        axios.post('http://localhost:3000/api/recipes', new_recette) // requete ajout de la nouvelle recette
             .then(response => {
                 recipes.push(response.data);
                 ajout_recette.reset();
